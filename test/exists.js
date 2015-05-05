a4bb03aaa1db358ca9a461f050dbe00b1f272f43
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-05 14:44:16 -0400

    all exists tests are passing

--- test/exists.js
+++ test/exists.js
@@ -8,18 +8,36 @@
 'use strict';
 
 require('should');
+var fs = require('fs');
 var path = require('path');
 var Tempdir = require('temporary/lib/dir');
 var tempdir = new Tempdir();
 var file = require('..');
 
+fs.symlinkSync(path.resolve('test/fixtures/octocat.png'), path.join(tempdir.path, 'octocat.png'), 'file');
+fs.symlinkSync(path.resolve('test/fixtures/expand'), path.join(tempdir.path, 'expand'), 'dir');
+
 describe('file.exists():', function () {
-  it('.exists():', function () {
-    test.ok(file.exists('test/fixtures/octocat.png'), 'files exist.');
-    test.ok(file.exists('test', 'fixtures', 'octocat.png'), 'should work for paths in parts.');
-    test.ok(file.exists('test/fixtures'), 'directories exist.');
-    test.ok(file.exists(path.join(tempdir.path, 'octocat.png')), 'file links exist.');
-    test.ok(file.exists(path.join(tempdir.path, 'expand')), 'directory links exist.');
-    test.equal(file.exists('test/fixtures/does/not/exist'), false, 'nonexistent files do not exist.');
+  it('should return `true` when a file exists:', function () {
+    file.exists('test/fixtures/octocat.png').should.be.true;
+  });
+  it('should work for paths passes as segments:', function () {
+    file.exists('test', 'fixtures', 'octocat.png').should.be.true;
+  });
+  it('should work for directories:', function () {
+    file.exists('test/fixtures').should.be.true;
+  });
+  it('should work for file symlinks:', function () {
+    file.exists(path.join(tempdir.path, 'octocat.png')).should.be.true;
+  });
+  it('should work for directory symlinks:', function () {
+    file.exists(path.join(tempdir.path, 'expand')).should.be.true;
+  });
+
+  it('should return `false` when a file does not exist:', function () {
+    file.exists('test/fixtures/does/not/exist/fooo.js').should.be.false;
+  });
+  it('should return `false` when a directory does not exist:', function () {
+    file.exists('test/fixtures/does/not/exist').should.be.false;
   });
 });
